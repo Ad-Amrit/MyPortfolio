bbda30f8ace0182fb84be5b6ccab26674d9c66f8
Ad-Amrit <amritadhikari.dev@gmail.com>
2024-04-29 16:18:39 +0545

    Home Section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,77 @@
 import React from 'react'
 import '../styles/Home.css'
+import Profile from '../assets/images/proff.png'
+import Typewriter from 'react-typewriter-effect';
 
 const Home = () => {
+  
   return (
-    <div>
-      <header>
-        <h1>Welcome to My Portfolio</h1>
-      </header>
-      <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing
-           elit. Corrupti tempora aliquam ullam quasi fuga
-            voluptatibus, sapiente numquam consequatur, eum,
-             adipisci iste. Facilis neque totam impedit fuga 
-             laborum necessitatibus reiciendis eum!</p>
-      </main>
+    <div className='Home'>
+      <div className='Main'>
+      <div className='TextSection'>
+        <h1>Hey There,</h1>
+        <p>IT'S ME AMRIT ADHIKARI</p>
+        <p>I'm a</p>
+        <Typewriter
+          options={{
+           strings: ['Software Engineer', 'Web Developer', 'React Developer'],
+           autoStart: true,
+           loop: true,
+          }}
+        />   
+        <p>As a recent graduate in software engineering, I'm brimming with
+           passion for web development and a thirst for knowledge. While I'm 
+           new to the professional world, I've been actively honing my
+           skills in HTML, CSS, and JavaScript. I'm particularly interested
+           in front-end development and creating user-friendly
+           interfaces.</p>
+      </div>
+      <div className='ImageSection'>
+        <img src={Profile} alt="Image" />
+      </div>
+      </div>
+      <div>
+        <h1>Languages</h1>
+        <div>
+          <section>
+             <img src="../src/assets/images/javascript.png" alt="Javascript" />
+             <p>JAVASCRIPT</p>
+          </section>
+        </div>
+      </div>
+
+      <div>
+        <h1>Front-end Tools</h1>
+        <div>
+          <section>
+            <img src="../src/assets/images/reactjs.png" alt="React" />
+            <p>React Js</p>
+          </section>
+          <section>
+            <img src="../src/assets/images/bootstrap.png" alt="Bootstrap" />
+            <p>Bootstrap</p>
+          </section>
+        </div>
+      </div>
+
+      <div>
+        <h1>Dev-Tools</h1>
+        <div>
+          <section>
+            <img src="../src/assets/images/git.png" alt="Git" />
+            <p>Git</p>
+          </section>
+          <section>
+            <img src="../src/assets/images/github.png" alt="GitHub" />
+            <p>Github</p>
+          </section>
+          <section>
+            <img src="../src/assets/images/npm.png" alt="npm" />
+            <p>npm</p>
+          </section>
+        </div>
+      </div>
+      
     </div>
   )
 }
